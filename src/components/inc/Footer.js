a60dc7cc2df8dcf6fd6673b5abe76e5017025d6a
iamnuru <abdulainurudeentitiaka@gmail.com>
2021-08-21 14:04:06 +0000

    update

--- src/components/inc/Footer.js
+++ src/components/inc/Footer.js
@@ -4,7 +4,7 @@ const Footer = ({theme: {light}}) => {
     <footer className={`page-footer ${light ? 'footer-lt' : 'footer-dt'}`}>
       <div className="footer-copyright">
         <div className="container text-center">
-          © 2021 All right reserved. version {process.env.REACT_APP_VERSION}. Open for Job. Email me via abdulainurudeentitiaka@gmail.com
+          © 2021 All right reserved. version <span style={{color:'rgb(240, 81, 107)'}}>{process.env.REACT_APP_VERSION}</span>. Open for Job. Email me via abdulainurudeentitiaka@gmail.com
         </div>
       </div>
     </footer>
